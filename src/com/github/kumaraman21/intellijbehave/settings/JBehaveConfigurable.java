bf42adbc0aa081bb1a0e38f8f9e87b547d81e633
mmerhav <Xla@123$$>
2018-04-15 16:46:11 +0300

    Fix stuff that should have been working (wtf)

--- src/com/github/kumaraman21/intellijbehave/settings/JBehaveConfigurable.java
+++ src/com/github/kumaraman21/intellijbehave/settings/JBehaveConfigurable.java
@@ -15,7 +15,6 @@
  */
 package com.github.kumaraman21.intellijbehave.settings;
 
-import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -23,68 +22,64 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
 public class JBehaveConfigurable implements SearchableConfigurable {
-  private JBehaveSettingsPanel jBehaveSettingsPanel;
 
-  @NotNull
-  @Override
-  public String getId() {
-    return getHelpTopic();
-  }
+	private JBehaveSettingsPanel jBehaveSettingsPanel;
 
-  @Override
-  public Runnable enableSearch(String s) {
-    return null;
-  }
+	@NotNull
+	@Override
+	public String getId() {
+		return getHelpTopic();
+	}
 
-  @Nls
-  @Override
-  public String getDisplayName() {
-    return "JBehave";
-  }
+	@Override
+	public Runnable enableSearch(String s) {
+		return null;
+	}
 
-  @Override
-  public Icon getIcon() {
-    return null;
-  }
+	@Nls
+	@Override
+	public String getDisplayName() {
+		return "JBehave";
+	}
 
-  @Override
-  public String getHelpTopic() {
-    return "reference.settingsdialog.project.jbehave";
-  }
+	@Override
+	public String getHelpTopic() {
+		return "reference.settingsdialog.project.jbehave";
+	}
 
-  @Override
-  public JComponent createComponent() {
-    if (jBehaveSettingsPanel == null) {
-      jBehaveSettingsPanel = new JBehaveSettingsPanel();
-    }
-    reset();
-    return jBehaveSettingsPanel.getContentPane();
-  }
+	@Override
+	public JComponent createComponent() {
+		if (jBehaveSettingsPanel == null) {
+			jBehaveSettingsPanel = new JBehaveSettingsPanel();
+		}
+		reset();
+		return jBehaveSettingsPanel.getContentPane();
+	}
 
-  @Override
-  public boolean isModified() {
-    if(jBehaveSettingsPanel != null) {
-      return jBehaveSettingsPanel.isModified();
-    }
-    return false;
-  }
+	@Override
+	public boolean isModified() {
+		if (jBehaveSettingsPanel != null) {
+			return jBehaveSettingsPanel.isModified();
+		}
+		return false;
+	}
 
-  @Override
-  public void apply() throws ConfigurationException {
-    if(jBehaveSettingsPanel != null) {
-      jBehaveSettingsPanel.apply();
-    }
-  }
+	@Override
+	public void apply() {
+		if (jBehaveSettingsPanel != null) {
+			jBehaveSettingsPanel.apply();
+		}
+	}
 
-  @Override
-  public void reset() {
-    if(jBehaveSettingsPanel != null) {
-      jBehaveSettingsPanel.reset();
-    }
-  }
+	@Override
+	public void reset() {
+		if (jBehaveSettingsPanel != null) {
+			jBehaveSettingsPanel.reset();
+		}
+	}
 
-  @Override
-  public void disposeUIResources() {
-    jBehaveSettingsPanel = null;
-  }
+	@Override
+	public void disposeUIResources() {
+		jBehaveSettingsPanel = null;
+	}
 }
